438ea99fe78c7d86510d361e9fe928f49d00757f
Fernando-MGS <70865380+Fernando-MGS@users.noreply.github.com>
2022-02-19 22:38:15 -0600

    Animaciones

--- PRACTICA 1/Web/src/index.js
+++ PRACTICA 1/Web/src/index.js
@@ -1,20 +1,44 @@
-const express = require('express');
-const app=express();
-const morgan = require ('morgan');
-const path = require ('path');
-//Settings
-app.set('port',process.env.PORT||3000)
-//Middlewares
-app.use(morgan('dev'));
+const express= require('express');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+const app= express();
+
+app.use(cors({
+    "methods": "GET,PUT,POST",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    credentials: true
+}));
+
+// parse requests of content-type - application/json
 app.use(express.json());
-//Routes
-app.use(require('./routes/task.route'));
-//Static files
- app.use(express.static(path.join(__dirname,'public')));
-console.log(__dirname+'\\public')
-//Starting server
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(app.get('port'),()=>{
-    console.log('server on port 3000');
+app.use(bodyParser.json());
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT} `));
+app.get('/',  function(req, res){
+    res.send(" WELCOME TO APP!");
+});
+
+app.get('/temperatura',  function(req, res){
+    data=  Math.random() * (60 - 15) + 15
+    
+    res.send(JSON.stringify(data));
+});
+
+app.get('/luz',  function(req, res){
+    data=  Math.random() * (500) + 0
+    
+    res.send(JSON.stringify(data));
+});
+
+app.get('/humedad',  function(req, res){
+    data=  Math.random() * (100) + 0
+    
+    res.send(JSON.stringify(data));
 });
