6779b00f2d013b7574d05060f9724385faf82beb
Fernando-MGS <70865380+Fernando-MGS@users.noreply.github.com>
2022-02-21 16:07:42 -0600

    Charts-Particulas

--- PRACTICA 1/Web/frontend/src/App.js
+++ PRACTICA 1/Web/frontend/src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import {UserData} from './Data'
 import BarChart from './components/BarChart';
 
 function App() {
+  //const [userData,setUserData] = useState();
+  useEffect(()=>{
+    try{
+      (async()=>{
+        const rawResponse=await fetch("http://localhost:8080/ChartTemp");
+        const content = await rawResponse.json();
+        console.log(content);
+        console.log("asdasd");
+        setUserData(content);
+      })();
+    }catch(error){
+      console.log("Algo salió mal");
+    }
+  });
 
   const [userData,setUserData] = useState({
     labels:UserData.map((data)=>data.year),
     datasets:[
       {
-        label: "Users Gaianed",
-        data: UserData.map((data)=>data.userGain)
+        label: "Users Gaianeds",
+        data: UserData.map((data)=>data.userGain),
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
       }
     ],
   });
--- PRACTICA 1/Web/src/index.js
+++ PRACTICA 1/Web/src/index.js
@@ -6,6 +6,40 @@ const { default: mongoose } = require('./database');
 const PORT = process.env.PORT || 8080;
 const app= express();
 
+const UserData = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823,
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345,
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555,
+    },
+    {
+      id: 4,
+      year: 2022,
+      userGain: 90000,
+      userLost: 4555,
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234,
+    },
+  ];
+
+
 app.use(cors({
     "methods": "GET,PUT,POST",
     "preflightContinue": false,
@@ -21,9 +55,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT} `));
+app.listen(PORT, ()=> console.log('Server running on port ${PORT} '));
 app.get('/',  function(req, res){
-    res.send(" WELCOME TO APP!");
+    res.send(" WELCOME TO APP1!");
 });
 
 app.use('/grafica', require('./routes/task.route'));
@@ -45,3 +79,30 @@ app.get('/humedad',  function(req, res){
     
     res.send(JSON.stringify(data));
 });
+
+app.get('/carbono',  function(req, res){
+  data=  Math.random() * (600) + 0  
+  res.send(JSON.stringify(data));
+});
+
+app.get('/velocidad',  function(req, res){
+  data= Math.random() * (100 - 1) + 1
+  
+  res.send(JSON.stringify(data));
+});
+
+app.get('/ChartTemp',  function(req, res){
+    const datos ={
+        labels:UserData.map((data)=>data.year),
+        datasets:[{
+            label:"Humedad del ambiente",
+            data:UserData.map((data)=>data.userGain),
+            fill:true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"}
+        ],
+    };
+    res.send(datos);
+});
+
+
