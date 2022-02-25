2d12f5db42770a9cc232ac2402ac835333aeb233
Fernando-MGS <70865380+Fernando-MGS@users.noreply.github.com>
2022-02-25 05:41:49 -0600

    Chart-Final

--- PRACTICA 1/Web/frontend/src/App.js
+++ PRACTICA 1/Web/frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState,useEffect,Fragment } from 'react';
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 import {UserData} from './Data'
 import BarChart from './components/BarChart';
 import Humedad from './components/Humedad';
@@ -18,14 +20,18 @@ function App() {
         const content = await rawResponse.json();
         console.log(content);
         console.log("asdasd");
-        setUserData(content);
+        setUserData(content[0]);
+        setUserData1(content[1]);
+        setUserData2(content[2]);
+        setUserData3(content[3]);
+        setUserData4(content[4]);
       })();
     }catch(error){
       console.log("Algo salió mal");
     }
   },[]);
 
-  const [userData,setUserData] = useState([{
+  const [userData,setUserData] = useState({
     labels:UserData.map((data)=>data.year),
     datasets:[
       {
@@ -36,16 +42,64 @@ function App() {
         borderColor: "rgba(75,192,192,1)"
       }
     ],
-  }]);
+  });
+  const [userData1,setUserData1] = useState({
+    labels:UserData.map((data)=>data.year),
+    datasets:[
+      {
+        label: "Users Gaianeds",
+        data: UserData.map((data)=>data.userGain),
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      }
+    ],
+  });
+  const [userData2,setUserData2] = useState({
+    labels:UserData.map((data)=>data.year),
+    datasets:[
+      {
+        label: "Users Gaianeds",
+        data: UserData.map((data)=>data.userGain),
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      }
+    ],
+  });
+  const [userData3,setUserData3] = useState({
+    labels:UserData.map((data)=>data.year),
+    datasets:[
+      {
+        label: "Users Gaianeds",
+        data: UserData.map((data)=>data.userGain),
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      }
+    ],
+  });
+  const [userData4,setUserData4] = useState({
+    labels:UserData.map((data)=>data.year),
+    datasets:[
+      {
+        label: "Users Gaianeds",
+        data: UserData.map((data)=>data.userGain),
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      }
+    ],
+  });
   return (
-    <div className="App">
-      aa
-      <BarChart chartData={userData}/>
-      <Humedad chartData={userData}/>
-      <Luz chartData={userData}/>
-      <TempExt chartData={userData}/>
-      <TempInt chartData={userData}/>
-    </div>
+    <Fragment>
+      a
+      <Line data={userData} />;   
+      <Line data={userData1} />;   
+      <Line data={userData2} />;   
+      <Line data={userData3} />;   
+      <Line data={userData4} />;   
+    </Fragment>
   );
 }
 
