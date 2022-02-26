47c8f17f794b5c40a54c4886e20736bdf3b49db3
JoshF8 <joshuafranco98@gmail.com>
2022-02-25 20:03:31 -0600

    Revert "Revert "Merge remote-tracking branch 'upstream/main'""
    This reverts commit 6a1340627e775c274098de5cff08224b6361241b.

--- PRACTICA 1/Web/frontend/src/components/TempInt.js
+++ PRACTICA 1/Web/frontend/src/components/TempInt.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function Humedad({ chartData }) {
-  return <Line data={chartData[0]} />;
+function TempInt({ chartData }) {
+  return <Line data={chartData[2]} />;
 }
 
-export default Humedad;
+export default TempInt;
