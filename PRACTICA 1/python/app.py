3b206077f7417f5cb24246d7641b0e31ed74dc70
JoshF8 <joshuafranco98@gmail.com>
2022-02-25 19:53:49 -0600

    python2

--- PRACTICA 1/python/app.py
+++ PRACTICA 1/python/app.py
@@ -11,18 +11,13 @@ client = MongoClient(mongodb_Host)
 
 #Datos para conexion con arduino
 puerto_serial = 'COM3'
-
+db = client[dbNombre]
+collection = db['Magnitudes']
 #Activacion de la conexion arduino
 arduino = serial.Serial(puerto_serial, 9600)
 time.sleep(3)
 while True:
    dato_leido = arduino.readline().decode('utf-8').strip()
+   collection.insert_one({'TemperaturaInterior': 20, 'TemperaturaExterior': 17, 'Luz':110, 'Humedad':40.7, 'CO2':18, 'Fecha': datetime.datetime.now()})
    print(dato_leido)
 
-
-##Dato leido sera lo que se guardara en la db, idealmente sera un texto convertible a array con coma o punto y coma
-
-#declaracion de la base de datos que se usa y de la coleccion
-db = client[dbNombre]
-collection = db['Magnitudes']
-collection.insert_one({'TemperaturaInterior': 20, 'TemperaturaExterior': 17, 'Luz':110, 'Humedad':40.7, 'CO2':18, 'Fecha': datetime.datetime.now()})
