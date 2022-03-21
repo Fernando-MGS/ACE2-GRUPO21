00fbb0835d96d114234b6a9f9299ed958b17bb0e
JoshF8 <joshuafranco98@gmail.com>
2022-03-20 23:19:07 -0600

    python

--- PROYECTO 1/python/app.py
+++ PROYECTO 1/python/app.py
@@ -23,21 +23,15 @@ collection.insert_one({'Suciedad1': 10, 'Humedad': 10, 'CantidadAgua':10, 'Sucie
 while True:
 
    dato_leido = arduino.readline().decode('utf-8')
-   aux=dato_leido.split(':')
-   if(aux[0] == "Magnitudes"):
-      aux2=aux[1].split(',')
-      print(aux2[0])
-      print(aux2[1])
-      print(aux2[2])
-      print(aux2[3])
+   aux=dato_leido.split(',')
+   print(aux[0])
+   print(aux[1])
+   print(aux[2])
+   print(aux[3])
    ##Dato leido sera lo que se guardara en la db, idealmente sera un texto convertible a array con coma o punto y coma
    #declaracion de la base de datos que se usa y de la coleccion
-      db = client[dbNombre]
-      collection = db['Magnitudes']
-      collection.insert_one({'Suciedad1': aux2[0], 'Humedad': aux2[1], 'CantidadAgua':aux2[2], 'Suciedad2':aux2[3],  'Fecha': datetime.datetime.now()})
-   else:
-      db = client[dbNombre]
-      collection = db['Tiempo']
-      collection.insert_one({'TiempoNecesario':aux[1]})
+   db = client[dbNombre]
+   collection = db['Magnitudes']
+   collection.insert_one({'Suciedad1': aux[0], 'Humedad': aux[1], 'CantidadAgua':aux[2], 'Suciedad2':aux[3],  'Fecha': datetime.datetime.now()})
 
   
